654afd3dd1d5b442aec90d3dff495590a3834420
Kvny404 <66086287+Kvny404@users.noreply.github.com>
2023-04-15 01:24:24 -0300

    Uncommented criarUser() method

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def signup():
         senha = request.form.get("senha")
         genero = request.form.get("genero")
         print(f'nome: {nome}\ndata nasc: {data_nascimento}\nemail: {email}\nsenha: {senha}\ngenero: {genero}')
-        #db.criarUser(name=nome, birthday=data_nascimento,email=email,password=senha,gender=genero)
+        db.criarUser(name=nome, birthday=data_nascimento,email=email,password=senha,gender=genero)
     return render_template("formulario.html")
 
 
